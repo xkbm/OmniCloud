8431d89843ab08af12068096811701dab10b1c2e
kbm_ <113648613+xkbm@users.noreply.github.com>
2026-08-17 14:38:21 -0400

    feat(storage): intercept large move transfers

--- worker/src/index.js
+++ worker/src/index.js
@@ -11,6 +11,7 @@ import {
 } from './auth.js';
 import { accountsRoutes } from './routes/accounts.js';
 import { allocationRoutes } from './routes/allocation.js';
+import { backgroundMoveRoutes } from './routes/backgroundMove.js';
 import { copyRoutes } from './routes/copy.js';
 import { filesRoutes } from './routes/files.js';
 import { googleRoutes } from './routes/google.js';
@@ -148,6 +149,7 @@ await allocationRoutes(app);
 await storageRoutes(app);
 await googleRoutes(app);
 await copyRoutes(app);
+await backgroundMoveRoutes(app);
 await moveRoutes(app);
 await filesRoutes(app);
 await transferRoutes(app);
